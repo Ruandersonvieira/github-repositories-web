7d0b8e0fc6cfbb99c62b64d96655f27d314005c1
Ruanderson Vieira <ruanderson@accalia.io>
2020-01-07 14:05:09 -0300

    :sparkles: add repositories by api

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,27 +1,69 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { FaGithub, FaSpinner , FaPlus } from 'react-icons/fa'
 
-import { FaGithub, FaPlus } from 'react-icons/fa'
+import api from '../../services/api';
 
 import { Container, Form, SubmitButton } from './styles';
 
-export default function  Main() {
+export default class Main extends Component {
+  state = {
+    newRepo : '',
+    repositories: [],
+    loading: 0,
+  }
+
+  handleInputChange = e => {
+    this.setState({ newRepo: e.target.value });
+  }
+
+  handleSubmit = async e => {
+    e.preventDefault();
+
+    this.setState({loading: 1});
+
+    const { newRepo, repositories } = this.state;
+
+    const response = await api.get(`repos/${newRepo}`);
+
+    const data = {
+      name: response.data.full_name,
+    }
+
+    this.setState({
+      repositories: [...repositories, data],
+      newRepo: '',
+      loading: 0,
+    })
+  }
+
+  render()  {
+  const { newRepo, loading } = this.state;
+
   return (
     <Container>
       <h1>
         <FaGithub/>
         Repositórios
       </h1>
-
-      <Form onSubmit={()=>{}}>
+      <Form onSubmit={this.handleSubmit}>
         <input
           type="text"
           placeholder="Adicionar repositório"
+          value={newRepo}
+          onChange={this.handleInputChange}
         />
-
-        <SubmitButton disabled>
-          <FaPlus color="#FFF" size={14}/>
+        <SubmitButton loading={loading}>
+          {
+            loading ? (
+              <FaSpinner color="#FFF" size={14}/>
+            )
+            : (
+              <FaPlus color="#FFF" size={14}/>
+            )
+          }
         </SubmitButton>
       </Form>
     </Container>
   );
+  }
 }
